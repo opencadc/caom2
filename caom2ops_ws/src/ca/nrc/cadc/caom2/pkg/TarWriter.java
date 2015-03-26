e1dde0065c62c309361e611e45eae283fbf7c707
Brian Major <major.brian@gmail.com>
2015-03-26 16:26:46 -0700

    s1707 - TarDownload unit test

--- caom2ops_ws/src/ca/nrc/cadc/caom2/pkg/TarWriter.java
+++ caom2ops_ws/src/ca/nrc/cadc/caom2/pkg/TarWriter.java
@@ -1,6 +1,25 @@
 package ca.nrc.cadc.caom2.pkg;
 
+import java.io.OutputStream;
+import java.net.URL;
+
 public class TarWriter
 {
+	
+	public TarWriter(OutputStream out)
+	{
+		
+		
+	}
+	
+	public void addSource(URL url)
+	{
+		
+	}
+	
+	public void write()
+	{
+		
+	}
 
 }
