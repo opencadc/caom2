ad35553121b54a58a3d6d9387e49ad2d3e9bae33
Jeff Burke <Jeff.Burke@nrc-cnrc.gc.ca>
2015-04-22 11:39:55 -0700

    s1813: access control process table and skeleton dao

---
+++ caom2ac/src/ca/nrc/cadc/caom/ac/dao/ACProcessDAOImpl.java
@@ -0,0 +1,104 @@
+/*
+ ************************************************************************
+ ****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
+ *
+ * (c) 2014.                            (c) 2014.
+ * National Research Council            Conseil national de recherches
+ * Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ * All rights reserved                  Tous droits reserves
+ *
+ * NRC disclaims any warranties         Le CNRC denie toute garantie
+ * expressed, implied, or statu-        enoncee, implicite ou legale,
+ * tory, of any kind with respect       de quelque nature que se soit,
+ * to the software, including           concernant le logiciel, y com-
+ * without limitation any war-          pris sans restriction toute
+ * ranty of merchantability or          garantie de valeur marchande
+ * fitness for a particular pur-        ou de pertinence pour un usage
+ * pose.  NRC shall not be liable       particulier.  Le CNRC ne
+ * in any event for any damages,        pourra en aucun cas etre tenu
+ * whether direct or indirect,          responsable de tout dommage,
+ * special or general, consequen-       direct ou indirect, particul-
+ * tial or incidental, arising          ier ou general, accessoire ou
+ * from the use of the software.        fortuit, resultant de l'utili-
+ *                                      sation du logiciel.
+ *
+ ****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
+ ************************************************************************
+ */
+
+package ca.nrc.cadc.caom.ac.dao;
+
+import ca.nrc.cadc.assetpermission.ArchiveProcess;
+import ca.nrc.cadc.caom.ac.ACProcess;
+import ca.nrc.cadc.caom.ac.server.ACProcessPersistence;
+
+import java.util.Collection;
+
+public class ACProcessDAOImpl implements ACProcessPersistence
+{
+    @Override
+    public ACProcess getACProcess(ACProcess acProcess)
+    {
+        try
+        {
+
+        }
+        catch(foo)
+        {
+
+        }
+    }
+
+    @Override
+    public Collection<ACProcess> getACProcesses()
+    {
+        try
+        {
+
+        }
+        catch(foo)
+        {
+
+        }
+    }
+
+    @Override
+    public ACProcess addACProcess(ACProcess acProcess)
+    {
+        try
+        {
+
+        }
+        catch(foo)
+        {
+
+        }
+    }
+
+    @Override
+    public ACProcess updateACProcess(ACProcess acProcess)
+    {
+        try
+        {
+
+        }
+        catch(foo)
+        {
+
+        }
+    }
+
+    @Override
+    public void deleteACProcess(ACProcess acProcess)
+    {
+        try
+        {
+
+        }
+        catch(foo)
+        {
+
+        }
+    }
+
+}
