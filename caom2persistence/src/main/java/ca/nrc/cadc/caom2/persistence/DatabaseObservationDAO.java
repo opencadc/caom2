79cfd03cff69a8ad5c7664571c9b91b2902441ea
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2016-11-30 13:45:39 -0800

    Revert "Story 1914. Updated copyright notice."
    This reverts commit 1837f65b9c6996dfff1f7c3df8155939d728ec24.

--- caom2persistence/src/main/java/ca/nrc/cadc/caom2/persistence/DatabaseObservationDAO.java
+++ caom2persistence/src/main/java/ca/nrc/cadc/caom2/persistence/DatabaseObservationDAO.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2016.                            (c) 2016.
+*  (c) 2011.                            (c) 2011.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -62,7 +62,7 @@
 *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
 *                                       <http://www.gnu.org/licenses/>.
 *
-*  $Revision: 5 $
+*  $Revision: 4 $
 *
 ************************************************************************
 */
