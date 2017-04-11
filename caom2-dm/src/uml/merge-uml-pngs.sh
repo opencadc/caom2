848cabe887cf6ec05cb47ee40f2b33cc27d01de1
Patrick Dowler <patrick.dowler@nrc-cnrc.gc.ca>
2017-04-11 10:42:21 -0700

    use project.version in xslt task to generate doc from current version; align uml diagrams better

--- caom2-dm/src/uml/merge-uml-pngs.sh
+++ caom2-dm/src/uml/merge-uml-pngs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-convert -append CAOM1core.png CAOM2datatypes.png CAOM3wcs.png CAOM4entities.png CAOM-2.3.png
+convert -append \
+	CAOM1core.png \
+	CAOM2datatypes.png \
+	CAOM3wcs.png \
+	CAOM4entities.png \
+	CAOM5vocabularies.png \
+	CAOM-2.3.png
 
