e9daf7355facc519e3307ab132077bf86857920b
jduran <jduran@dhcp-10-66-197-11.esac.esa.int>
2017-06-13 15:17:42 +0200

    getting observations list is now parallelized in RepoClient

--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClient.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClient.java
@@ -2,7 +2,6 @@ package ca.nrc.cadc.caom2.repo.client;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -14,10 +13,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.security.auth.Subject;
 
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import ca.nrc.cadc.auth.AuthMethod;
 import ca.nrc.cadc.auth.AuthenticationUtil;
@@ -25,14 +30,15 @@ import ca.nrc.cadc.auth.RunnableAction;
 import ca.nrc.cadc.caom2.Observation;
 import ca.nrc.cadc.caom2.ObservationState;
 import ca.nrc.cadc.caom2.ObservationURI;
-import ca.nrc.cadc.caom2.xml.ObservationParsingException;
-import ca.nrc.cadc.caom2.xml.ObservationReader;
+import ca.nrc.cadc.caom2.persistence.DatabaseObservationDAO;
+import ca.nrc.cadc.caom2.persistence.SQLGenerator;
+import ca.nrc.cadc.caom2.persistence.Util;
 import ca.nrc.cadc.date.DateUtil;
 import ca.nrc.cadc.net.HttpDownload;
 import ca.nrc.cadc.reg.Standards;
 import ca.nrc.cadc.reg.client.RegistryClient;
 
-public class RepoClient {
+public class RepoClient extends DatabaseObservationDAO {
 
     private static final Logger log = Logger.getLogger(RepoClient.class);
     private final DateFormat df = DateUtil.getDateFormat(DateUtil.IVOA_DATE_FORMAT, DateUtil.UTC);
@@ -43,6 +49,7 @@ public class RepoClient {
     private Subject subject = null;
     private AuthMethod meth;
     private String collection = null;
+    private int nthreads = 1;
 
     protected final String BASE_HTTP_URL;
 
@@ -51,7 +58,8 @@ public class RepoClient {
     }
 
     // constructor takes service identifier arg
-    public RepoClient(URI resourceID, String collection) {
+    public RepoClient(URI resourceID, String collection, int nthreads) {
+        this.nthreads = nthreads;
         this.resourceId = resourceID;
 
         rc = new RegistryClient();
@@ -76,8 +84,9 @@ public class RepoClient {
 
     }
 
+    @Override
     public List<ObservationState> getObservationList(String collection, Date start, Date end,
-            Integer maxrec) throws ParseException {
+            Integer maxrec) {
         // Use HttpDownload to make the http GET calls (because it handles a lot of the
         // authentication stuff)
 
@@ -108,8 +117,132 @@ public class RepoClient {
             e.printStackTrace();
         }
 
-        return transformByteArrayOutputStreamIntoListOfObservationState(collection, bos, df, ',',
-                '\n');
+        List<ObservationState> list;
+        try {
+            list = transformByteArrayOutputStreamIntoListOfObservationState(collection, bos, df,
+                    ',', '\n');
+        } catch (ParseException e) {
+            throw new RuntimeException("Unable to list of ObservationState from " + bos.toString());
+        }
+        return list;
+    }
+
+    public Iterator<Observation> observationIterator() {
+        return null;
+
+    }
+
+    @Override
+    public void setConfig(Map<String, Object> config1) {
+
+    }
+
+    @Override
+    public List<Observation> getList(Class<Observation> c, Date startDate, Date end,
+            Integer numberOfObservations, int depth) {
+
+        List<Observation> list = new ArrayList<Observation>();
+
+        List<ObservationState> stateList = getObservationList(collection, startDate, end,
+                numberOfObservations);
+
+        ByteArrayOutputStream bos = null;
+        // Create tasks for each file
+        List<Callable<Observation>> tasks = new ArrayList<Callable<Observation>>();
+
+        for (ObservationState os : stateList) {
+            tasks.add(new WorkerThread(os, subject, BASE_HTTP_URL, collection, bos));
+        }
+
+        // Run tasks in a fixed thread pool
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(nthreads);
+        List<Future<Observation>> futures;
+        try {
+            futures = taskExecutor.invokeAll(tasks);
+        } catch (InterruptedException e1) {
+            throw new RuntimeException("Unable to create ExecutorService");
+        }
+
+        for (Future<Observation> f : futures) {
+            Observation res = null;
+            try {
+                res = f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("Unable execute thread");
+            }
+            if (f.isDone()) {
+                list.add(res);
+            }
+        }
+        taskExecutor.shutdown();
+
+        List<ObservationState> erroneousObservations = checkResults(list, stateList);
+
+        if (erroneousObservations.size() > 0) {
+            String erroneous = "";
+            for (ObservationState os : erroneousObservations) {
+                erroneous += os.getObservationID() + " ";
+            }
+            throw new RuntimeException("errors reading observations: " + erroneous);
+        }
+
+        return list;
+    }
+
+    private List<ObservationState> checkResults(List<Observation> observationList,
+            List<ObservationState> stateList) {
+        List<ObservationState> erroneous = new ArrayList<ObservationState>();
+
+        boolean found = false;
+        for (ObservationState os : stateList) {
+            found = false;
+            for (Observation o : observationList) {
+                if (o.getObservationID().equals(os.getObservationID())) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                erroneous.add(os);
+            }
+        }
+        return erroneous;
+    }
+
+    @Override
+    public UUID getID(ObservationURI uri) {
+        Observation observation = get(uri, null, 1);
+        if (observation != null)
+            return observation.getID();
+        return null;
+    }
+
+    @Override
+    public ObservationURI getURI(UUID id) {
+        Observation observation = get(null, id, 1);
+        if (observation != null)
+            return observation.getURI();
+        return null;
+    }
+
+    @Override
+    public Observation get(UUID id) {
+        if (id == null)
+            throw new IllegalArgumentException("id cannot be null");
+        return get(null, id, SQLGenerator.MAX_DEPTH);
+    }
+
+    /**
+     * Get a stored observation by URI.
+     *
+     * @param uri
+     * @return the complete observation
+     */
+    @Override
+    public Observation get(ObservationURI uri) {
+        if (uri == null)
+            throw new IllegalArgumentException("uri cannot be null");
+        return get(uri, null, SQLGenerator.MAX_DEPTH);
     }
 
     private List<ObservationState> transformByteArrayOutputStreamIntoListOfObservationState(
@@ -139,72 +272,48 @@ public class RepoClient {
         return list;
     }
 
-    public Observation get(ObservationURI obs) {
-        return null;
+    private Observation get(ObservationURI uri, UUID id, int depth) {
+        checkInit();
+        if (uri == null && id == null)
+            throw new IllegalArgumentException("args cannot be null");
+        log.debug("GET: " + uri);
+        long t = System.currentTimeMillis();
 
-    }
+        try {
+            String sql;
+            if (uri != null)
+                sql = gen.getSelectSQL(uri, depth);
+            else
+                sql = gen.getSelectSQL(id, depth, false);
 
-    public Iterator<Observation> observationIterator() {
-        return null;
+            if (log.isDebugEnabled())
+                log.debug("GET: " + Util.formatSQL(sql));
 
-    }
-
-    public void setConfig(Map<String, Object> config1) {
-
-    }
-
-    public List<Observation> getList(Class<Observation> c, Date startDate, Date end,
-            int numberOfObservations)
-            throws ParseException, IOException, ObservationParsingException {
-
-        List<Observation> list = new ArrayList<Observation>();
-
-        List<ObservationState> stateList = getObservationList(collection, startDate, end,
-                numberOfObservations);
-
-        ByteArrayOutputStream bos = null;
-
-        for (ObservationState os : stateList) {
-            String id = os.getObservationID();
-
-            bos = new ByteArrayOutputStream();
-            String surl = BASE_HTTP_URL + File.separator + collection + File.separator + id;
-            URL url;
-            try {
-                url = new URL(surl);
-                HttpDownload get = new HttpDownload(url, bos);
-
-                if (subject != null) {
-                    Subject.doAs(subject, new RunnableAction(get));
-
-                    log.info("Query run within subject");
-                } else {
-                    get.run();
-                    log.info("Query run");
-                }
-
-            } catch (MalformedURLException e) {
-                log.error("Exception in getList: " + e.getMessage());
-                e.printStackTrace();
-                throw e;
+            JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+            Object result = jdbc.query(sql, gen.getObservationExtractor());
+            if (result == null)
+                return null;
+            if (result instanceof List) {
+                List obs = (List) result;
+                if (obs.isEmpty())
+                    return null;
+                if (obs.size() > 1)
+                    throw new RuntimeException(
+                            "BUG: get " + uri + " query returned " + obs.size() + " observations");
+                Object o = obs.get(0);
+                if (o instanceof Observation) {
+                    Observation ret = (Observation) obs.get(0);
+                    return ret;
+                } else
+                    throw new RuntimeException(
+                            "BUG: query returned an unexpected type " + o.getClass().getName());
             }
-            log.info("ID = " + id + " = \n" + bos.toString());
-
-            ObservationReader obsReader = new ObservationReader();
-            Observation o = obsReader.read(bos.toString());
-
-            list.add(o);
+            throw new RuntimeException(
+                    "BUG: query returned an unexpected type " + result.getClass().getName());
+        } finally {
+            long dt = System.currentTimeMillis() - t;
+            log.debug("GET: " + uri + " " + dt + "ms");
         }
-
-        return list;
-    }
-
-    public ObservationURI getURI(UUID curID) {
-        return null;
-    }
-
-    public Observation get(UUID skipID) {
-        return null;
     }
 
 }
--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClientMainTest.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClientMainTest.java
@@ -1,10 +1,7 @@
 package ca.nrc.cadc.caom2.repo.client;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.ParseException;
 import java.util.List;
 
 import javax.security.auth.Subject;
@@ -16,7 +13,6 @@ import ca.nrc.cadc.auth.AuthenticationUtil;
 import ca.nrc.cadc.auth.RunnableAction;
 import ca.nrc.cadc.caom2.Observation;
 import ca.nrc.cadc.caom2.ObservationState;
-import ca.nrc.cadc.caom2.xml.ObservationParsingException;
 import ca.nrc.cadc.net.NetrcAuthenticator;
 import ca.nrc.cadc.util.Log4jInit;
 
@@ -49,34 +45,26 @@ public class RepoClientMainTest implements Runnable {
 
     @Override
     public void run() {
+
+        RepoClient repoC = null;
         try {
-            RepoClient repoC = new RepoClient(new URI("ivo://cadc.nrc.ca/caom2repo"), "IRIS");
-
-            List<ObservationState> list = repoC.getObservationList("IRIS", null, null, 5);
-
-            for (ObservationState os : list) {
-                log.info(os.toString());
-            }
-
-            List<Observation> l = repoC.getList(Observation.class, null, null, 5);
-
-            for (Observation o : l) {
-                log.info(o.toString());
-            }
-
+            repoC = new RepoClient(new URI("ivo://cadc.nrc.ca/caom2repo"), "IRIS", 8);
         } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ObservationParsingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new RuntimeException(
+                    "Unable to create RepoClient instance for URI ivo://cadc.nrc.ca/caom2repo and collection IRIS");
+
+        }
+
+        List<ObservationState> list = repoC.getObservationList("IRIS", null, null, 5);
+
+        for (ObservationState os : list) {
+            log.info(os.toString());
+        }
+
+        List<Observation> l = repoC.getList(Observation.class, null, null, 5);
+
+        for (Observation o : l) {
+            log.info(o.toString());
         }
 
     }
