53d9f3b60180bc36dd82d0e9087c2516536a7935
jduran <jduran@dhcp-10-66-197-11.esac.esa.int>
2017-06-15 16:23:21 +0200

    Changes following Pat's comments

--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/ObservationState.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/ObservationState.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2017.                            (c) 2017.
+*  (c) 2011.                            (c) 2011.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -70,83 +70,47 @@
 package ca.nrc.cadc.caom2.repo.client;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.UUID;
+import java.util.Date;
 
-import javax.security.auth.Subject;
+/**
+ * Wrapper class to support listing observations in incremental mode.
+ *
+ * @author pdowler
+ */
+public class ObservationState {
+    // private static final Logger log =
+    // Logger.getLogger(ObservationState.class);
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+    private final String collection;
+    private final String observationID;
+    private final Date maxLastModified;
+    private final URI accMetaChecksum;
 
-import ca.nrc.cadc.auth.AuthenticationUtil;
-import ca.nrc.cadc.auth.RunnableAction;
-import ca.nrc.cadc.caom2.Observation;
-import ca.nrc.cadc.caom2.ObservationURI;
-import ca.nrc.cadc.net.NetrcAuthenticator;
-import ca.nrc.cadc.util.Log4jInit;
-
-public class RepoClientMainTest implements Runnable {
-
-    private static final Logger log = Logger.getLogger(RepoClient.class);
-
-    // constructor takes service identifier arg
-    public RepoClientMainTest() {
-
+    public ObservationState(String collection, String observationID, Date maxlastModified, URI accMetaChecksum) {
+	this.collection = collection;
+	this.observationID = observationID;
+	this.maxLastModified = maxlastModified;
+	this.accMetaChecksum = accMetaChecksum;
     }
 
-    public static void main(String[] args) {
+    public String getCollection() {
+	return collection;
+    }
 
-	Log4jInit.setLevel("ca.nrc.cadc.caom2.repo.client.RepoClient", Level.DEBUG);
-	// Log4jInit.setLevel("ca.nrc.cadc.net.HttpDownload", Level.TRACE);
-	// Log4jInit.setLevel("ca.nrc.cadc.reg.client.RegistryClient",
-	// Level.DEBUG);
-	// Log4jInit.setLevel("ca.nrc.cadc.net.NetrcAuthenticator",
-	// Level.DEBUG);
-	// Log4jInit.setLevel("ca.nrc.cadc.net.NetrcFile", Level.DEBUG);
-	// Log4jInit.setLevel("ca.nrc.cadc.auth.AuthenticationUtil",
-	// Level.DEBUG);
+    public String getObservationID() {
+	return observationID;
+    }
 
-	// Create a subject
-	Subject s = AuthenticationUtil.getSubject(new NetrcAuthenticator(true));
-	RepoClientMainTest rct = new RepoClientMainTest();
-	if (s != null) {
-	    Subject.doAs(s, new RunnableAction(rct));
-	}
+    public Date getMaxLastModified() {
+	return maxLastModified;
+    }
 
+    public URI getAccMetaChecksum() {
+	return accMetaChecksum;
     }
 
     @Override
-    public void run() {
-
-	RepoClient repoC = null;
-	try {
-	    repoC = new RepoClient(new URI("ivo://cadc.nrc.ca/caom2repo"), "IRIS", 8);
-	} catch (URISyntaxException e) {
-	    throw new RuntimeException(
-		    "Unable to create RepoClient instance for URI ivo://cadc.nrc.ca/caom2repo and collection IRIS");
-
-	}
-
-	List<ObservationState> list = repoC.getObservationList("IRIS", null, null, 5);
-
-	for (ObservationState os : list) {
-	    log.info(os.toString());
-	}
-
-	List<Observation> l = repoC.getList(Observation.class, null, null, 5, 1);
-
-	for (Observation o : l) {
-	    log.info(o.getID() + " " + o.getCollection() + " " + o.getURI().toString() + " " + o.getLastModified());
-	}
-
-	Observation o = repoC.get(new ObservationURI("IRIS", "f005h000"));
-
-	// repoC.get(new UUID(l.get(0).getID().getMostSignificantBits(),
-	// l.get(0).getID().getLeastSignificantBits()));
-
-	UUID uuid = repoC.getID(new ObservationURI("IRIS", "f005h000"));
-
+    public String toString() {
+	return "ObservationState[" + observationID + "," + maxLastModified + "]";
     }
-
 }
