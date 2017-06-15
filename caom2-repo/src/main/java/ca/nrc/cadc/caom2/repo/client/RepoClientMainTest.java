5461cbed2896df10a621fa4d3bb30e4a194bfe7e
jduran <jduran@dhcp-10-66-197-11.esac.esa.int>
2017-06-15 16:23:21 +0200

    Changes following Pat's comments

--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClientMainTest.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClientMainTest.java
@@ -1,3 +1,72 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2017.                            (c) 2017.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 5 $
+*
+************************************************************************
+*/
+
 package ca.nrc.cadc.caom2.repo.client;
 
 import java.net.URI;
@@ -13,7 +82,6 @@ import org.apache.log4j.Logger;
 import ca.nrc.cadc.auth.AuthenticationUtil;
 import ca.nrc.cadc.auth.RunnableAction;
 import ca.nrc.cadc.caom2.Observation;
-import ca.nrc.cadc.caom2.ObservationState;
 import ca.nrc.cadc.caom2.ObservationURI;
 import ca.nrc.cadc.net.NetrcAuthenticator;
 import ca.nrc.cadc.util.Log4jInit;
@@ -29,53 +97,55 @@ public class RepoClientMainTest implements Runnable {
 
     public static void main(String[] args) {
 
-        Log4jInit.setLevel("ca.nrc.cadc.caom2.repo.client.RepoClient", Level.DEBUG);
-        // Log4jInit.setLevel("ca.nrc.cadc.net.HttpDownload", Level.TRACE);
-        // Log4jInit.setLevel("ca.nrc.cadc.reg.client.RegistryClient", Level.DEBUG);
-        // Log4jInit.setLevel("ca.nrc.cadc.net.NetrcAuthenticator", Level.DEBUG);
-        // Log4jInit.setLevel("ca.nrc.cadc.net.NetrcFile", Level.DEBUG);
-        // Log4jInit.setLevel("ca.nrc.cadc.auth.AuthenticationUtil", Level.DEBUG);
+	Log4jInit.setLevel("ca.nrc.cadc.caom2.repo.client.RepoClient", Level.DEBUG);
+	// Log4jInit.setLevel("ca.nrc.cadc.net.HttpDownload", Level.TRACE);
+	// Log4jInit.setLevel("ca.nrc.cadc.reg.client.RegistryClient",
+	// Level.DEBUG);
+	// Log4jInit.setLevel("ca.nrc.cadc.net.NetrcAuthenticator",
+	// Level.DEBUG);
+	// Log4jInit.setLevel("ca.nrc.cadc.net.NetrcFile", Level.DEBUG);
+	// Log4jInit.setLevel("ca.nrc.cadc.auth.AuthenticationUtil",
+	// Level.DEBUG);
 
-        // Create a subject
-        Subject s = AuthenticationUtil.getSubject(new NetrcAuthenticator(true));
-        RepoClientMainTest rct = new RepoClientMainTest();
-        if (s != null) {
-            Subject.doAs(s, new RunnableAction(rct));
-        }
+	// Create a subject
+	Subject s = AuthenticationUtil.getSubject(new NetrcAuthenticator(true));
+	RepoClientMainTest rct = new RepoClientMainTest();
+	if (s != null) {
+	    Subject.doAs(s, new RunnableAction(rct));
+	}
 
     }
 
     @Override
     public void run() {
 
-        RepoClient repoC = null;
-        try {
-            repoC = new RepoClient(new URI("ivo://cadc.nrc.ca/caom2repo"), "IRIS", 8);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(
-                    "Unable to create RepoClient instance for URI ivo://cadc.nrc.ca/caom2repo and collection IRIS");
+	RepoClient repoC = null;
+	try {
+	    repoC = new RepoClient(new URI("ivo://cadc.nrc.ca/caom2repo"), "IRIS", 8);
+	} catch (URISyntaxException e) {
+	    throw new RuntimeException(
+		    "Unable to create RepoClient instance for URI ivo://cadc.nrc.ca/caom2repo and collection IRIS");
 
-        }
+	}
 
-        List<ObservationState> list = repoC.getObservationList("IRIS", null, null, 5);
+	List<ObservationState> list = repoC.getObservationList("IRIS", null, null, 5);
 
-        for (ObservationState os : list) {
-            log.info(os.toString());
-        }
+	for (ObservationState os : list) {
+	    log.info(os.toString());
+	}
 
-        List<Observation> l = repoC.getList(Observation.class, null, null, 5, 1);
+	List<Observation> l = repoC.getList(Observation.class, null, null, 5, 1);
 
-        for (Observation o : l) {
-            log.info(o.getID() + " " + o.getCollection() + " " + o.getURI().toString() + " "
-                    + o.getLastModified());
-        }
+	for (Observation o : l) {
+	    log.info(o.getID() + " " + o.getCollection() + " " + o.getURI().toString() + " " + o.getLastModified());
+	}
 
-        Observation o = repoC.get(new ObservationURI("IRIS", "f005h000"));
+	Observation o = repoC.get(new ObservationURI("IRIS", "f005h000"));
 
-        // repoC.get(new UUID(l.get(0).getID().getMostSignificantBits(),
-        // l.get(0).getID().getLeastSignificantBits()));
+	// repoC.get(new UUID(l.get(0).getID().getMostSignificantBits(),
+	// l.get(0).getID().getLeastSignificantBits()));
 
-        UUID uuid = repoC.getID(new ObservationURI("IRIS", "f005h000"));
+	UUID uuid = repoC.getID(new ObservationURI("IRIS", "f005h000"));
 
     }
 
