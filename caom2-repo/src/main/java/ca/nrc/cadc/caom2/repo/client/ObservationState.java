e0d3d7f45449c87c51f850a19a0cd9973f2d0f57
jduran <jduran@dhcp-10-66-197-123.esac.esa.int>
2017-06-29 10:04:54 +0200

    Indentation fixed

--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/ObservationState.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/ObservationState.java
@@ -79,54 +79,54 @@ import java.util.Date;
  */
 public class ObservationState
 {
-	// private static final Logger log =
-	// Logger.getLogger(ObservationState.class);
+    // private static final Logger log =
+    // Logger.getLogger(ObservationState.class);
 
-	private final String collection;
-	private final String observationID;
-	private final Date maxLastModified;
-	private final URI accMetaChecksum;
-	private final URI uri;
+    private final String collection;
+    private final String observationID;
+    private final Date maxLastModified;
+    private final URI accMetaChecksum;
+    private final URI uri;
 
-	public ObservationState(String collection, String observationID,
-			Date maxlastModified, URI accMetaChecksum, URI uri)
-	{
-		this.collection = collection;
-		this.observationID = observationID;
-		this.maxLastModified = maxlastModified;
-		this.accMetaChecksum = accMetaChecksum;
-		this.uri = uri;
-	}
+    public ObservationState(String collection, String observationID,
+            Date maxlastModified, URI accMetaChecksum, URI uri)
+    {
+        this.collection = collection;
+        this.observationID = observationID;
+        this.maxLastModified = maxlastModified;
+        this.accMetaChecksum = accMetaChecksum;
+        this.uri = uri;
+    }
 
-	public String getCollection()
-	{
-		return collection;
-	}
+    public String getCollection()
+    {
+        return collection;
+    }
 
-	public String getObservationID()
-	{
-		return observationID;
-	}
+    public String getObservationID()
+    {
+        return observationID;
+    }
 
-	public Date getMaxLastModified()
-	{
-		return maxLastModified;
-	}
+    public Date getMaxLastModified()
+    {
+        return maxLastModified;
+    }
 
-	public URI getAccMetaChecksum()
-	{
-		return accMetaChecksum;
-	}
+    public URI getAccMetaChecksum()
+    {
+        return accMetaChecksum;
+    }
 
-	@Override
-	public String toString()
-	{
-		return "ObservationState[" + observationID + "," + maxLastModified
-				+ "]";
-	}
+    @Override
+    public String toString()
+    {
+        return "ObservationState[" + observationID + "," + maxLastModified
+                + "]";
+    }
 
-	public URI getUri()
-	{
-		return uri;
-	}
+    public URI getUri()
+    {
+        return uri;
+    }
 }
--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/WorkerResponse.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/WorkerResponse.java
@@ -74,60 +74,60 @@ import ca.nrc.cadc.caom2.Observation;
 public class WorkerResponse
 {
 
-	private Observation observation = null;
-	private ObservationState observationState = null;
-	private Exception error = null;
+    private Observation observation = null;
+    private ObservationState observationState = null;
+    private Exception error = null;
 
-	public WorkerResponse(Observation obs, ObservationState obsState,
-			Exception err)
-	{
-		this.setObservation(obs);
-		this.setObservationState(obsState);
-		this.setError(err);
-	}
+    public WorkerResponse(Observation obs, ObservationState obsState,
+            Exception err)
+    {
+        this.setObservation(obs);
+        this.setObservationState(obsState);
+        this.setError(err);
+    }
 
-	public Observation getObservation()
-	{
-		return observation;
-	}
+    public Observation getObservation()
+    {
+        return observation;
+    }
 
-	public void setObservation(Observation observation)
-	{
-		this.observation = observation;
-	}
+    public void setObservation(Observation observation)
+    {
+        this.observation = observation;
+    }
 
-	public ObservationState getObservationState()
-	{
-		return observationState;
-	}
+    public ObservationState getObservationState()
+    {
+        return observationState;
+    }
 
-	public void setObservationState(ObservationState observationState)
-	{
-		this.observationState = observationState;
-	}
+    public void setObservationState(ObservationState observationState)
+    {
+        this.observationState = observationState;
+    }
 
-	public Exception getError()
-	{
-		return error;
-	}
+    public Exception getError()
+    {
+        return error;
+    }
 
-	public void setError(Exception error)
-	{
-		this.error = error;
-	}
+    public void setError(Exception error)
+    {
+        this.error = error;
+    }
 
-	@Override
-	public String toString()
-	{
-		return observation == null
-				? "null"
-				: observation.getObservationID() + " "
-						+ observationState == null
-								? "null"
-								: observationState.getCollection() + " "
-										+ error == null
-												? "Correct"
-												: error.getMessage();
-	}
+    @Override
+    public String toString()
+    {
+        return observation == null
+                ? "null"
+                : observation.getObservationID() + " "
+                        + observationState == null
+                                ? "null"
+                                : observationState.getCollection() + " "
+                                        + error == null
+                                                ? "Correct"
+                                                : error.getMessage();
+    }
 
 }
