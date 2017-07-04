64dde5449e279abc5fc8d18aaa774a47e86d915d
jduran <jduran@dhcp-10-66-197-123.esac.esa.int>
2017-07-04 15:13:20 +0200

    Initial commit for caom2harvester-validation

---
+++ caom2harvester-validation/src/main/java/ca/nrc/cadc/caom2/harvester/validation/ServiceDeletedEntityDAO.java
@@ -0,0 +1,39 @@
+package ca.nrc.cadc.caom2.harvester.validation;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import ca.nrc.cadc.caom2.DeletedEntity;
+import ca.nrc.cadc.caom2.persistence.AbstractDAO;
+
+public class ServiceDeletedEntityDAO<T extends DeletedEntity> extends AbstractDAO
+{
+
+    private static final Logger log = Logger.getLogger(ServiceDeletedEntityDAO.class);
+
+    public ServiceDeletedEntityDAO()
+    {
+    }
+
+    public List<T> getList(Class<? extends DeletedEntity> c, Date minLastModified, Date maxLastModified,
+            Integer batchSize)
+    {
+        List<T> ret = new ArrayList<T>();
+        log.debug("GET: " + batchSize);
+        long t = System.currentTimeMillis();
+        try
+        {
+
+        }
+        finally
+        {
+            long dt = System.currentTimeMillis() - t;
+            log.debug("GET: " + batchSize + " " + dt + "ms");
+        }
+        return ret;
+    }
+
+}
