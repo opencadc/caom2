012b598f82329703c0f93dcc0a6c187295a39faa
jduran <jduran@localhost>
2017-07-18 01:29:19 +0200

    Format corrections. Optimizations in RepoClient.getObservationList()

--- caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClient.java
+++ caom2-repo/src/main/java/ca/nrc/cadc/caom2/repo/client/RepoClient.java
@@ -1,51 +1,71 @@
 /*
- ************************************************************************
- ******************* CANADIAN ASTRONOMY DATA CENTRE ******************* CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES
- * **************
- *
- * (c) 2011. (c) 2011. Government of Canada Gouvernement du Canada National Research Council Conseil
- * national de recherches Ottawa, Canada, K1A 0R6 Ottawa, Canada, K1A 0R6 All rights reserved Tous
- * droits réservés
- *
- * NRC disclaims any warranties, Le CNRC dénie toute garantie expressed, implied, or énoncée,
- * implicite ou légale, statutory, of any kind with de quelque nature que ce respect to the
- * software, soit, concernant le logiciel, including without limitation y compris sans restriction
- * any warranty of merchantability toute garantie de valeur or fitness for a particular marchande ou
- * de pertinence purpose. NRC shall not be pour un usage particulier. liable in any event for any Le
- * CNRC ne pourra en aucun cas damages, whether direct or être tenu responsable de tout indirect,
- * special or general, dommage, direct ou indirect, consequential or incidental, particulier ou
- * général, arising from the use of the accessoire ou fortuit, résultant software. Neither the name
- * de l'utilisation du logiciel. Ni of the National Research le nom du Conseil National de Council
- * of Canada nor the Recherches du Canada ni les noms names of its contributors may de ses
- * participants ne peuvent be used to endorse or promote être utilisés pour approuver ou products
- * derived from this promouvoir les produits dérivés software without specific prior de ce logiciel
- * sans autorisation written permission. préalable et particulière par écrit.
- *
- * This file is part of the Ce fichier fait partie du projet OpenCADC project. OpenCADC.
- *
- * OpenCADC is free software: OpenCADC est un logiciel libre ; you can redistribute it and/or vous
- * pouvez le redistribuer ou le modify it under the terms of modifier suivant les termes de the GNU
- * Affero General Public la “GNU Affero General Public License as published by the License” telle
- * que publiée Free Software Foundation, par la Free Software Foundation either version 3 of the :
- * soit la version 3 de cette License, or (at your option) licence, soit (à votre gré) any later
- * version. toute version ultérieure.
- *
- * OpenCADC is distributed in the OpenCADC est distribué hope that it will be useful, dans l’espoir
- * qu’il vous but WITHOUT ANY WARRANTY; sera utile, mais SANS AUCUNE without even the implied
- * GARANTIE : sans même la garantie warranty of MERCHANTABILITY implicite de COMMERCIALISABILITÉ or
- * FITNESS FOR A PARTICULAR ni d’ADÉQUATION À UN OBJECTIF PURPOSE. See the GNU Affero PARTICULIER.
- * Consultez la Licence General Public License for Générale Publique GNU Affero more details. pour
- * plus de détails.
- *
- * You should have received Vous devriez avoir reçu une a copy of the GNU Affero copie de la Licence
- * Générale General Public License along Publique GNU Affero avec with OpenCADC. If not, see
- * OpenCADC ; si ce n’est <http://www.gnu.org/licenses/>. pas le cas, consultez :
- * <http://www.gnu.org/licenses/>.
- *
- * $Revision: 5 $
- *
- ************************************************************************
- */
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2011.                            (c) 2011.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 5 $
+*
+************************************************************************
+*/
 
 package ca.nrc.cadc.caom2.repo.client;
 
@@ -65,7 +85,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -150,10 +169,9 @@ public class RepoClient
     {
         init();
 
-        List<ObservationState> list = new ArrayList<ObservationState>();
-        TreeSet<ObservationState> accSet = new TreeSet<ObservationState>(uriComparator);
-        List<ObservationState> partialSet = null;
-        boolean tooBigRequest = maxrec == null || maxrec <= MAX_NUMBER;
+        List<ObservationState> accList = new ArrayList<ObservationState>();
+        List<ObservationState> partialList = null;
+        boolean tooBigRequest = maxrec == null || maxrec > MAX_NUMBER;
 
         Integer rec = maxrec;
         Integer recCounter = 0;
@@ -174,7 +192,7 @@ public class RepoClient
                 go = false;// only one go
             String surl = surlCommon;
             if (rec != null)
-                surl = surl + "?maxRec=" + rec;
+                surl = surl + "?maxRec=" + (rec + 1);
             if (start != null)
                 surl = surl + "&start=" + df.format(start);
             if (end != null)
@@ -201,8 +219,13 @@ public class RepoClient
 
             try
             {
-                partialSet = transformByteArrayOutputStreamIntoListOfObservationState(bos, df, '\t', '\n');
-                accSet.addAll(partialSet);
+                partialList = transformByteArrayOutputStreamIntoListOfObservationState(bos, df, '\t', '\n');
+                if (partialList != null && accList != null && !partialList.isEmpty() && !accList.isEmpty() && accList.get(accList.size() - 1).equals(partialList.get(0)))
+                {
+                    partialList.remove(0);
+                }
+
+                accList.addAll(partialList);
                 bos.close();
             }
             catch (ParseException | IOException e)
@@ -210,15 +233,20 @@ public class RepoClient
                 throw new RuntimeException("Unable to list of ObservationState from " + bos.toString(), e);
             }
 
-            start = accSet.last().maxLastModified;
-            recCounter += partialSet.size();
-            if (partialSet.size() == 1 && (maxrec != null && recCounter >= maxrec) || (start != null && end != null && start.equals(end)) || partialSet.size() < rec)
+            start = accList.get(accList.size() - 1).maxLastModified;
+            recCounter = accList.size();
+            if (maxrec != null && maxrec - recCounter < rec)
+            {
+                rec = maxrec - recCounter;
+            }
+            log.info("dinamic batch (rec): " + rec);
+
+            if (accList.isEmpty() || (maxrec != null && recCounter >= maxrec) || (start != null && end != null && start.equals(end)) || partialList.size() < rec)
             {
                 go = false;
             }
         }
-        list.addAll(accSet);
-        return list;
+        return accList;
     }
 
     public Iterator<Observation> observationIterator()
