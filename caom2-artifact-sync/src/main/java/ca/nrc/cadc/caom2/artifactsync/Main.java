977e2becfd460c92f1a7f51d3b23e85652ebf946
Brian Major <major.brian@gmail.com>
2017-08-03 14:05:20 -0700

    CADC Implementation of ArtifactStore

--- caom2-artifact-sync/src/main/java/ca/nrc/cadc/caom2/artifactsync/Main.java
+++ caom2-artifact-sync/src/main/java/ca/nrc/cadc/caom2/artifactsync/Main.java
@@ -1,3 +1,72 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2017.                            (c) 2017.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 5 $
+*
+************************************************************************
+*/
+
 package ca.nrc.cadc.caom2.artifactsync;
 
 import java.net.URI;
@@ -15,8 +84,10 @@ import ca.nrc.cadc.caom2.repo.client.RepoClient;
 import ca.nrc.cadc.net.NetrcAuthenticator;
 import ca.nrc.cadc.util.ArgumentMap;
 import ca.nrc.cadc.util.Log4jInit;
+import ca.nrc.cadc.util.PropertiesReader;
 
 /**
+ * Command line entry point for running the caom2-artifact-sync tool.
  *
  * @author majorb
  */
@@ -86,14 +157,17 @@ public class Main
             }
 
             int nthreads = 1;
-            try
+            if (am.isSet("threads"))
             {
-                nthreads = Integer.parseInt(am.getValue("threads"));
-            }
-            catch (NumberFormatException nfe)
-            {
-                log.error("Illegal value for --threads: " + am.getValue("threads"));
-                System.exit(-1);
+                try
+                {
+                    nthreads = Integer.parseInt(am.getValue("threads"));
+                }
+                catch (NumberFormatException nfe)
+                {
+                    log.error("Illegal value for --threads: " + am.getValue("threads"));
+                    System.exit(-1);
+                }
             }
 
             boolean detectMissing = am.isSet("detect-missing");
@@ -107,17 +181,7 @@ public class Main
             exitValue = 2; // in case we get killed
             Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownHook()));
 
-            ArtifactStore artifactStore = null;
-            try
-            {
-                Class<?> asClass = Class.forName(ArtifactStore.class.getName() + "Impl");
-                artifactStore = (ArtifactStore) asClass.newInstance();
-            }
-            catch (Exception e)
-            {
-                log.error("Failed to load " + ArtifactStore.class.getName() + "Impl", e);
-                System.exit(-1);
-            }
+            ArtifactStore artifactStore = loadArtifictStoreImpl();
 
             Runnable worker = null;
             if (detectMissing)
@@ -129,7 +193,7 @@ public class Main
 
                 URI uresourceID = URI.create(sresourceId);
                 RepoClient caom2client = new RepoClient(uresourceID, 1);
-                worker = new DetectMissingArtifacts(caom2client, artifactStore, scollection, dryrun);
+                worker = new DetectMissingArtifacts(caom2client, artifactStore, scollection);
             }
             else
             {
@@ -156,6 +220,22 @@ public class Main
         finally
         {
             System.exit(exitValue);
+        }
+    }
+
+    private static ArtifactStore loadArtifictStoreImpl()
+    {
+        try
+        {
+            PropertiesReader pr = new PropertiesReader("ArtifactStore.properties");
+            String asClassName = pr.getFirstPropertyValue("ArtifactStore.impl.class");
+            log.debug("Artifact store class: " + asClassName);
+            Class<?> asClass = Class.forName(asClassName);
+            return (ArtifactStore) asClass.newInstance();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Failed to load " + ArtifactStore.class.getName() + "Impl", e);
         }
     }
 
