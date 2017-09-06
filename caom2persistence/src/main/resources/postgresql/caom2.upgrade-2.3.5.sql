72924835c6acf9742b6b1d44c721a9e0a3f6138a
Patrick Dowler <patrick.dowler@nrc-cnrc.gc.ca>
2017-09-06 11:41:46 -0700

    add schema to drop index statements

--- caom2persistence/src/main/resources/postgresql/caom2.upgrade-2.3.5.sql
+++ caom2persistence/src/main/resources/postgresql/caom2.upgrade-2.3.5.sql
@@ -5,11 +5,13 @@ drop view if exists caom2.ObsCore;
 drop view if exists caom2.SIAv1;
 
 -- bug fix: correct handling of keywords
-drop index if exists Observation_i_instr_kw;
-drop index if exists Observation_i_prop_kw;
-drop index if exists Observation_i_targ_kw;
-drop index if exists Observation_i_tel_kw;
+drop index caom2.Observation_i_instr_kw;
+drop index caom2.Observation_i_prop_kw;
+drop index caom2.Observation_i_targ_kw;
+drop index caom2.Observation_i_tel_kw;
+drop index caom2.Plane_i_prov_kw;
 
+-- keyword handling change: tsvector to citext with | separator
 alter table caom2.Observation
     alter column instrument_keywords type citext
         using replace(replace(instrument_keywords::varchar,$$'$$,''),' ','|'),
@@ -20,27 +22,24 @@ alter table caom2.Observation
     alter column telescope_keywords type citext
         using replace(replace(telescope_keywords::varchar,$$'$$,''),' ','|');
 
-drop index if exists Plane_i_prov_kw;
-
+--\echo altering keywords column (plane)...
 alter table caom2.Plane
     alter column provenance_keywords type citext
         using replace(replace(provenance_keywords::varchar,$$'$$,''),' ','|');
 
+-- bug fix: rename columns to match model
 alter table caom2.Plane
     rename column position_dimension1 to position_dimension_naxis1;
 alter table caom2.Plane
     rename column position_dimension2 to position_dimension_naxis2;
 
--- bug fix: correct support for IVOA/DALI datatypes
+-- bug fix: correct support for IVOA/DALI datatypes and richer caom2 polygons
 alter table caom2.Plane
     add column position_bounds_points double precision[],
     add column position_bounds_samples double precision[],
     add column energy_bounds_samples polygon,
     add column time_bounds_samples polygon;
 
+-- preserve bounds values we previously stored in the wrong column
 update caom2.Plane 
     set (energy_bounds_samples,time_bounds_samples) = (energy_bounds, time_bounds);
-
--- recreate indices on keywords? NO - need a large database before I can study
--- the possible query plans
-
