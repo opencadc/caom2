2127d384d42e2108fb4275e14e3d563ed8a9bf48
Patrick Dowler <patrick.dowler@nrc-cnrc.gc.ca>
2018-03-09 12:48:41 -0800

    rename CAOM adql query processor class; keep old class for backwards compat but deprecate; add optional ctor so subclasses can disable the CaomReadAccessConverter

--- caom2-tap-server/src/main/java/ca/nrc/cadc/tap/impl/AdqlQueryImpl.java
+++ caom2-tap-server/src/main/java/ca/nrc/cadc/tap/impl/AdqlQueryImpl.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2016.                            (c) 2016.
+*  (c) 2018.                            (c) 2018.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -62,97 +62,22 @@
 *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
 *                                       <http://www.gnu.org/licenses/>.
 *
-*  $Revision: 5 $
-*
 ************************************************************************
 */
 
 package ca.nrc.cadc.tap.impl;
 
-import ca.nrc.cadc.tap.AdqlQuery;
-import ca.nrc.cadc.tap.caom2.CaomSelectListConverter;
-import ca.nrc.cadc.tap.caom2.CaomReadAccessConverter;
-import ca.nrc.cadc.tap.caom2.CaomRegionConverter;
-import ca.nrc.cadc.tap.caom2.IsDownloadableConverter;
-import ca.nrc.cadc.tap.parser.BaseExpressionDeParser;
-import ca.nrc.cadc.tap.parser.PgsphereDeParser;
-import ca.nrc.cadc.tap.parser.converter.TableNameConverter;
-import ca.nrc.cadc.tap.parser.converter.TableNameReferenceConverter;
-import ca.nrc.cadc.tap.parser.converter.TopConverter;
-import ca.nrc.cadc.tap.parser.converter.postgresql.PgFunctionNameConverter;
-import ca.nrc.cadc.tap.parser.extractor.FunctionExpressionExtractor;
-import ca.nrc.cadc.tap.parser.navigator.ExpressionNavigator;
-import ca.nrc.cadc.tap.parser.navigator.FromItemNavigator;
-import ca.nrc.cadc.tap.parser.navigator.ReferenceNavigator;
-import ca.nrc.cadc.tap.parser.navigator.SelectNavigator;
-import net.sf.jsqlparser.util.deparser.SelectDeParser;
-import org.apache.log4j.Logger;
-
 /**
- * AdqlQuery implementation for PostgreSQL + pg_sphere + CAOM-2.
+ * The original class of this name has been renamed to CaomAdqlQuery. This temporary 
+ * class is here to support backwards compatibility and will be removed in future.
  * 
+ * @deprecated use CaomAdqlQuery 
  * @author pdowler
  */
-public class AdqlQueryImpl extends AdqlQuery
-{
-    private static Logger log = Logger.getLogger(AdqlQueryImpl.class);
-    
-    public AdqlQueryImpl() { }
-    
-    @Override
-    protected void init()
-    {
-        super.init();
+@Deprecated
+public class AdqlQueryImpl extends CaomAdqlQuery {
 
-        // convert TOP -> LIMIT
-        super.navigatorList.add(new TopConverter(
-                new ExpressionNavigator(), new ReferenceNavigator(), new FromItemNavigator()));
-
-        // convert ADQL geometry function calls to alternate form
-        super.navigatorList.add(new CaomRegionConverter());
-        
-        // convert functions to PG-specific names
-        navigatorList.add(new FunctionExpressionExtractor(
-            new PgFunctionNameConverter(), new ReferenceNavigator(), new FromItemNavigator()));
-
-        // after CaomRegionConverter: triggering off the same column names and converting some uses
-        TableNameConverter tnc = new TableNameConverter(true);
-        tnc.put("ivoa.ObsCore", "caom2.ObsCore");
-        tnc.put("ivoa.ObsFile", "caom2.ObsFile");
-        tnc.put("ivoa.ObsPart", "caom2.ObsPart");
-        // TAP-1.1 version of tap_schema
-        tnc.put("tap_schema.schemas", "tap_schema.schemas11");
-        tnc.put("tap_schema.tables", "tap_schema.tables11");
-        tnc.put("tap_schema.columns", "tap_schema.columns11");
-        tnc.put("tap_schema.keys", "tap_schema.keys11");
-        tnc.put("tap_schema.key_columns", "tap_schema.key_columns11");
-        TableNameReferenceConverter tnrc = new TableNameReferenceConverter(tnc.map);
-        super.navigatorList.add(new SelectNavigator(new ExpressionNavigator(), tnrc, tnc));
-
-        // enforce access control policy in queries - must be after TableNameConverter
-        super.navigatorList.add(new CaomReadAccessConverter());
-
-        // convert use of the isDownloadable function
-        super.navigatorList.add(new IsDownloadableConverter());
-        
-        // change caom2.Artifact.accessURL to caom2.Artifact.uri
-        super.navigatorList.add(new CaomSelectListConverter());
-        
-        //for (Object o : navigatorList)
-        //    log.debug("navigator: " + o.getClass().getName());
-    }
-    
-    @Override
-    protected BaseExpressionDeParser getExpressionDeparser(SelectDeParser dep, StringBuffer sb)
-    {
-        return new PgsphereDeParser(dep, sb);
-    }
-    
-    @Override
-    public String getSQL()
-    {
-        String sql = super.getSQL();
-        log.debug("SQL:\n" + sql); 
-        return sql;
+    public AdqlQueryImpl() { 
+        super();
     }
 }
