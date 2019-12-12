dc83c1d053063a0df97a03d27762a6b9a34a321e
Patrick Dowler <pdowler.cadc@gmail.com>
2019-12-12 13:23:50 -0800

    changed energy_emband to energy_energyBands in EnumField (materialised) view

--- caom2-tap-server/src/main/resources/sql/caom2.EnumField_view.sql
+++ caom2-tap-server/src/main/resources/sql/caom2.EnumField_view.sql
@@ -8,11 +8,11 @@ drop view if exists caom2.EnumField;
 create view caom2.EnumField as 
 select count(*) as num_tuples, max(p.time_bounds_lower) as max_time_bounds_cval1,  
     o.collection, o.telescope_name, o.instrument_name, o.type,o.intent,
-    p.dataProductType, p.calibrationLevel, p.energy_emband, p.energy_bandpassName
+    p.dataProductType, p.calibrationLevel, p.energy_energyBands, p.energy_bandpassName
 from caom2.Observation o join caom2.Plane p on o.obsID=p.obsID
 where o.collection != 'TEST'
 group by o.collection, o.telescope_name, o.instrument_name, o.type, o.intent,
-    p.dataProductType, p.calibrationLevel, p.energy_emband, p.energy_bandpassName
+    p.dataProductType, p.calibrationLevel, p.energy_energyBands, p.energy_bandpassName
 ;
 
 grant select on caom2.EnumField to CVOPUB;
