aceac85168912f9b2b65230a93105adfc36fc365
Jeff Burke <jburke.cadc@gmail.com>
2023-06-29 10:36:10 -0700

    CADC-12390 copy caom2-repo-server action classes to torkeep and rename package

---
+++ torkeep/src/main/java/org/opencadc/torkeep/action/GetDeletedAction.java
@@ -0,0 +1,149 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2017.                            (c) 2017.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+*/
+
+package org.opencadc.torkeep.action;
+
+import ca.nrc.cadc.caom2.DeletedObservation;
+import ca.nrc.cadc.caom2.persistence.DeletedEntityDAO;
+import ca.nrc.cadc.date.DateUtil;
+import ca.nrc.cadc.io.ByteCountOutputStream;
+import ca.nrc.cadc.net.ResourceNotFoundException;
+import com.csvreader.CsvWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author pdowler
+ */
+public class GetDeletedAction extends RepoAction {
+    private static final Logger log = Logger.getLogger(GetDeletedAction.class);
+
+    public GetDeletedAction() { 
+    }
+
+    @Override
+    public void doAction() throws Exception {
+        // lazy: this currently does not check if extra path elements were included
+        if (getCollection() != null) {
+            InputParams ip = getInputParams();
+            doList(ip.maxrec, ip.start, ip.end, ip.ascending);
+        } else {
+            // Responds to requests where no collection is provided.
+            // Returns list of all collections.
+            doGetCollectionList();
+        }
+    }
+
+    protected void doList(int maxRec, Date start, Date end, boolean isAscending) throws Exception {
+        log.debug("START: " + getCollection());
+
+        checkReadPermission();
+
+        DeletedEntityDAO dao = getDeletedDAO();
+
+        List<DeletedObservation> dels = dao.getList(getCollection(), start, end, maxRec);
+
+        if (dels == null) {
+            throw new ResourceNotFoundException("Collection not found: " + getCollection());
+        }
+
+        long byteCount = writeDeleted(dels);
+        logInfo.setBytes(byteCount);
+
+        log.debug("DONE: " + getCollection());
+    }
+    
+    private long writeDeleted(List<DeletedObservation> dels) throws IOException {
+        DateFormat df = DateUtil.getDateFormat(DateUtil.IVOA_DATE_FORMAT, DateUtil.UTC);
+        syncOutput.setHeader("Content-Type", "text/tab-separated-values");
+        
+        OutputStream os = syncOutput.getOutputStream();
+        ByteCountOutputStream bc = new ByteCountOutputStream(os);
+        OutputStreamWriter out = new OutputStreamWriter(bc, "US-ASCII");
+        CsvWriter writer = new CsvWriter(out, '\t');
+        for (DeletedObservation ddo : dels) {
+            writer.write(ddo.getID().toString());
+            writer.write(ddo.getURI().getCollection());
+            writer.write(ddo.getURI().getObservationID());
+            if (ddo.lastModified != null) {
+                writer.write(df.format(ddo.lastModified));
+            } else {
+                writer.write("");
+            }
+            writer.endRecord();
+        }
+        writer.flush();
+        return bc.getByteCount();
+    }
+    
+}
